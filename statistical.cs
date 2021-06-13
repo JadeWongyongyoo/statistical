491ac2ed3cfcc12c1dca6a7c1df720c8b8832a82
JadeWongyoungyoo <nawaf.w1@gmail.com>
2021-06-13 15:34:32 +0700

    statistical

---
+++ statistical.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FinalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input             
+            string inputstring = Console.ReadLine();
+            while (inputstring != "End")
+            {
+                string inputnumber = Console.ReadLine();
+            }
+            if (inputstring == "End")
+            {
+                string inputFind = Console.ReadLine();
+                while (inputFind != "FindMax" || inputFind != "FindMin" || inputFind != "FindMean")
+                {
+                    Console.WriteLine("Invalid mode");
+                    inputFind = Console.ReadLine();
+                }
+                    if (inputFind == "FindMax")
+                    {
+                        //int X = Math.Max(inputstring);
+                        //Console.WriteLine(X);
+                    }
+                    else if (inputFind == "FindMin")
+                    {
+                        //int X = Math.Min(inputstring);
+                        //Console.WriteLine(X);
+                    }
+                    else if (inputFind == "FindMean")
+                    {
+                        //double X = Math.Mean(inputstring);
+                        //Console.WriteLine(X);
+                    }
+                
+
+            }
+        }
+        
+    }
+}
